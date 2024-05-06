6b26e486bef779ecaf1e99dc577ab61fa940c0c2
Owlrazum <owlrazum@gmail.com>
2024-05-06 20:38:02 +0600

    initial rust swift integration

---
+++ src/lib.rs
@@ -0,0 +1,96 @@
+use std::{
+    fs::File,
+    io::{Read, Write},
+};
+
+use tink_core::keyset::{JsonReader, Reader};
+
+uniffi::include_scaffolding!("saead"); // "example" is the name of the .udl file 
+
+const CHUNK_SIZE: usize = 4096;
+
+fn encrypt(
+    source_file_path: String, 
+    dist_file_path: String, 
+    key_file_path: String,
+    aad: String,
+) -> String {
+
+    //https://github.com/project-oak/tink-rust/blob/main/streaming/README.md
+
+    tink_streaming_aead::init();
+
+    let mut reader = JsonReader::new(File::open(key_file_path).expect("fkey_open"));
+    let keyset = reader.read().expect("fjson_read");
+    // Generate fresh key material.
+    tink_core::keyset::Handle::new_with_no_secrets(keyset).expect("fhandle");
+    let kh = tink_core::keyset::Handle::new(
+            &tink_streaming_aead::aes128_gcm_hkdf_4kb_key_template()
+        ).expect("f_handle");
+
+    // Get the primitive that uses the key material.
+    let a = tink_streaming_aead::new(&kh).expect("f_primitive");
+
+    let ct_file = std::fs::File::create(source_file_path.clone()).expect("fcreate");
+    let mut w = a.new_encrypting_writer(Box::new(ct_file), aad.as_bytes()).expect("fwriter");
+
+    let mut buffer = Vec::new();
+    let mut pt_file = std::fs::File::open(dist_file_path).expect("fopen");
+    let ct_size = pt_file.read_to_end(&mut buffer).expect("fread_to_end");
+      // Write data to the encrypting-writer, in chunks to simulate streaming.
+    let mut offset = 0;
+    while offset < ct_size {
+        let end = std::cmp::min(ct_size, offset + CHUNK_SIZE);
+        let written = w.write(&buffer[offset..end]).expect("fwrite_encrypt");
+        offset += written;
+        // Can flush but it does nothing.
+        w.flush().expect("fflush");
+    }
+    // Complete the encryption (process any remaining buffered plaintext).
+    w.close().expect("fclose");
+
+    return "OK".to_string();
+}
+
+
+fn decrypt(
+    source_file_path: String, 
+    dist_file_path: String, 
+    key_file_path: String,
+    aad: String,
+) -> String {
+
+    //https://github.com/project-oak/tink-rust/blob/main/streaming/README.md
+
+    tink_streaming_aead::init();
+
+    let mut reader = JsonReader::new(File::open(key_file_path).expect("fkey_open"));
+    let keyset = reader.read().expect("fjson_read");
+    // Generate fresh key material.
+    tink_core::keyset::Handle::new_with_no_secrets(keyset).expect("fhandle");
+    let kh = tink_core::keyset::Handle::new(
+            &tink_streaming_aead::aes128_gcm_hkdf_4kb_key_template()
+        ).expect("f_handle");
+
+    // Get the primitive that uses the key material.
+    let a = tink_streaming_aead::new(&kh).expect("f_primitive");
+
+    let ct_file = std::fs::File::open(source_file_path).expect("fopen");
+    let mut r = a.new_decrypting_reader(Box::new(ct_file), aad.as_bytes()).expect("freader");
+
+    // Read data from the decrypting-reader, in chunks to simulate streaming.
+    let mut recovered = vec![];
+    loop {
+        let mut chunk = vec![0; CHUNK_SIZE];
+        let len = r.read(&mut chunk).expect("fdecrypt");
+        if len == 0 {
+            break;
+        }
+        recovered.extend_from_slice(&chunk[..len]);
+    }
+
+    let mut dist_file = std::fs::File::create(dist_file_path.clone()).expect("fopen");
+    dist_file.write_all(&recovered).expect("fwrite");
+    
+    return "OK".to_string();
+}

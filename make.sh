2216886057b6b480dc0e8a595b25944b983d7f4b
Owlrazum <owlrazum@gmail.com>
2024-05-13 19:11:24 +0600

    Remove simulator support and x64 support, add --release flag to cargobuild in make.sh

--- make.sh
+++ make.sh
@@ -2,8 +2,8 @@ set -e # Helps to give error info
 
 # Project paths
 
-RUST_PROJ="/Users/abai/Documents/Freelance/rust_saead/saead"
-IOS_LIB="/Users/abai/Documents/Freelance/rust_saead/saead/saeadLib"
+RUST_PROJ="/Users/abai/Documents/Freelance/saead"
+IOS_LIB="/Users/abai/Documents/Freelance/saead/saeadLib"
 
 LOCAL_UDL="src/saead.udl"
 UDL_NAME="saead"
@@ -16,19 +16,20 @@ PATH="$PATH:/usr/local/bin" # Adds swiftformat to the path
 
 cd "$RUST_PROJ"
 
-# Compile the rust
+# Compile the rust in debug version
 cargo build --target aarch64-apple-ios
-cargo build --target aarch64-apple-ios-sim
-cargo build --target x86_64-apple-ios
+#cargo build --release --target aarch64-apple-ios
+# cargo build --target aarch64-apple-ios-sim
+#cargo build --target x86_64-apple-ios
 
 # Remove old files if they exist
 IOS_ARM64_FRAMEWORK="$FRAMEWORK_NAME.xcframework/ios-arm64/$FRAMEWORK_NAME.framework"
-IOS_SIM_FRAMEWORK="$FRAMEWORK_NAME.xcframework/ios-arm64_x86_64-simulator/$FRAMEWORK_NAME.framework"
+# IOS_SIM_FRAMEWORK="$FRAMEWORK_NAME.xcframework/ios-arm64_x86_64-simulator/$FRAMEWORK_NAME.framework"
 
 rm -f "$IOS_ARM64_FRAMEWORK/$FRAMEWORK_NAME"
 rm -f "$IOS_ARM64_FRAMEWORK/Headers/${UDL_NAME}FFI.h"
-rm -f "$IOS_SIM_FRAMEWORK/$FRAMEWORK_NAME"
-rm -f "$IOS_SIM_FRAMEWORK/Headers/${UDL_NAME}FFI.h"
+# rm -f "$IOS_SIM_FRAMEWORK/$FRAMEWORK_NAME"
+# rm -f "$IOS_SIM_FRAMEWORK/Headers/${UDL_NAME}FFI.h"
 
 rm -f target/universal.a
 rm -f include/ios/*
@@ -40,22 +41,22 @@ mkdir -p include/ios
 cargo run --bin uniffi-bindgen generate "$LOCAL_UDL" --language swift --out-dir ./include/ios
 
 # Make fat lib for sims
-lipo -create \
-    "target/aarch64-apple-ios-sim/debug/lib${UDL_NAME}.a" \
-    "target/x86_64-apple-ios/debug/lib${UDL_NAME}.a" \
-    -output target/universal.a
+#lipo -create \
+#    -output target/universal.a
+    #"target/x86_64-apple-ios/debug/lib${UDL_NAME}.a" \
+    #"target/aarch64-apple-ios-sim/debug/lib${UDL_NAME}.a" \
 
 # Move binaries
 cp "target/aarch64-apple-ios/debug/lib${UDL_NAME}.a" \
     "$IOS_ARM64_FRAMEWORK/$FRAMEWORK_NAME"
-cp target/universal.a \
-    "$IOS_SIM_FRAMEWORK/$FRAMEWORK_NAME"
+#cp target/universal.a \
+#    "$IOS_SIM_FRAMEWORK/$FRAMEWORK_NAME"
 
 # Move headers
 cp "include/ios/${UDL_NAME}FFI.h" \
     "$IOS_ARM64_FRAMEWORK/Headers/${UDL_NAME}FFI.h"
-cp "include/ios/${UDL_NAME}FFI.h" \
-    "$IOS_SIM_FRAMEWORK/Headers/${UDL_NAME}FFI.h"
+#cp "include/ios/${UDL_NAME}FFI.h" \
+#    "$IOS_SIM_FRAMEWORK/Headers/${UDL_NAME}FFI.h"
 
 # Move swift interface
 sed "s/${UDL_NAME}FFI/$FRAMEWORK_NAME/g" "include/ios/$UDL_NAME.swift" > "include/ios/$SWIFT_INTERFACE.swift"
